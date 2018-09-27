af8c8b3936309b5d26dc87d3ce46dd339fa97644
armorsteven <43592967+armorsteven@users.noreply.github.com>
2018-09-27 12:25:42 -0500

    Update Elevator.h

--- Elevator.h
+++ Elevator.h
@@ -1,4 +1,3 @@
-
 #pragma once
 #ifndef ELEVATOR_H
 #define ELEVATOR_H
@@ -6,26 +5,43 @@
 #include "stdafx.h"
 #include <iostream>
 #include <string>
-using namespace std; 
+using namespace std;
 
-struct Elevator(){
-	string tendency;
-  int current_floor;
-  ListOfStop<int>;
-  ListOfInSideRequest<int>;
+struct Elevator {
+	string direction;
+	int current_floor;
+
+	ListOfStop Elevator_1;
+	ListOfInSideRequest Elevator_1;
+	ListOfOutsideRequest Elevator_1;
 
 };
 
-struct ListOfStop{
-int FloorToStop;
-ListOfStop*next, prev;
+struct ListOfStop {
+	int FloorToStop;
+	ListOfStop* next;
+	ListOfStop* prev;
 
-ListOfStop(const int& data_item, ListOfStop* prev_val =NULL, ListOfStop* next_val = NULL) :FloorToStop(data_item), prev(prev_val), next(next_val) {}
+	ListOfStop(const int& data_item, ListOfStop* prev_val = NULL, ListOfStop* next_val = NULL) :FloorToStop(data_item), prev(prev_val), next(next_val) {}
 };
 
-struct ListOfInSideRequest{
-int FloorForRequest;
-ListOfInSideRequest*next, prev;
+struct ListOfInSideRequest {
+	int FloorRequested;
+	ListOfInSideRequest* next;
+	ListOfInSideRequest* prev;
 
-ListOfInSideRequest(const int& data_item, ListOfInSideRequest* prev_val =NULL, ListOfInSideRequest* next_val = NULL) :FloorForRequest(data_item), prev(prev_val), next(next_val) {}
+	ListOfInSideRequest(const int& data_item, ListOfInSideRequest* prev_val = NULL, ListOfInSideRequest* next_val = NULL) :FloorForRequest(data_item), prev(prev_val), next(next_val) {}
 };
+
+struct ListOfOutsideRequest
+{
+	int floorAt;
+	string direction;
+
+	ListOfOutsideRequest(int floor_at, string direct) {};
+	ListOfOutsideRequest(int floor_at = NULL, string direct = NULL);
+
+};
+
+
+#endif
