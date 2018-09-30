59d0901f8b52227bfc701f12f3dd5a3ba95ec77a
Leo12Chen <43522679+Leo12Chen@users.noreply.github.com>
2018-09-30 18:08:37 -0500

    Update and rename SingleElevator to SingleElevator.cpp

---
+++ SingleElevator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+#include "Elevator.h"
+
+using namepace std;
+
+int main(){
+
+struct Node{
+int data;
+Node*next;
+Node*prev;
+Node(const  int&data_item, Node*prev_val = NULL, DNode*next_val = Null): data(data_item),prev(prev_val),next(next_val){}
+};
+
+Node<int>*
+
+return 0;
+}
