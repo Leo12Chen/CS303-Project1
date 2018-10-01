980d798384e5b9c234f879ab39d24fe669c7574d
Leo12Chen <43522679+Leo12Chen@users.noreply.github.com>
2018-09-30 20:06:37 -0500

    Create Request.h

---
+++ Request.h
@@ -0,0 +1,19 @@
+#pragma once
+#ifndef REQUEST_H
+#define REQUEST_H
+
+#include "stdafx.h"
+#include "List.h"
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Request {
+	string direction;                 //the direction of the Request is going;
+	int current_floor;                //the current floor of Request is at;
+};
+
+
+
+
+#endif
