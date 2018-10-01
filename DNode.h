15f4b6227729227d30fdf63191bbe66a7f760da9
armorsteven <43592967+armorsteven@users.noreply.github.com>
2018-09-30 21:43:26 -0500

    Update DNode.h

--- DNode.h
+++ DNode.h
@@ -8,7 +8,7 @@ struct DNode {
 		int data;
 		DNode* next;
 		DNode* prev;
-		DNode(const Item_Type& data_item, DNode* prev_val = NULL, DNode* next_val = NULL) :
+		DNode(const int& data_item, DNode* prev_val = NULL, DNode* next_val = NULL) :
 			data(data_item), prev(prev_val), next(next_val) {}
 	}; 
   
