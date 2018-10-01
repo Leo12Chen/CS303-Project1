afc832124aa7635166453632c4077d13afbf7357
Leo12Chen <43522679+Leo12Chen@users.noreply.github.com>
2018-09-30 19:24:39 -0500

    Update Elevator.h

--- Elevator.h
+++ Elevator.h
@@ -9,11 +9,11 @@
 using namespace std;
 
 struct Elevator {
-	string direction;
-	int current_floor;
-        List<int> ListOfStop;
-	List<int> InSideRequestList;
-	Queue<int> PendingQueue;
+	string direction;                 //the direction of the Elevator is going;
+	int current_floor;                //the current floor of Elevator is at;
+        List<int> ListOfStop;             //List of the floors which Elevator will stop
+	List<int> InSideRequestList;      //List of the floors which request by people in the elevator
+	Queue<int> PendingQueue;          //Queue of the floors which are pending 
 };
 
 
