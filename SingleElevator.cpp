35516226a745f67f597b157547a5df6525fb5cc9
Leo12Chen <43522679+Leo12Chen@users.noreply.github.com>
2018-10-05 18:41:24 -0500

    Update SingleElevator.cpp

--- SingleElevator.cpp
+++ SingleElevator.cpp
@@ -27,11 +27,82 @@ Basic algorithm of the code:
 #include "List.h"
 #include "Elevator.h"
 #include "Request.h"
+#include "Queue.h"
 
 using namepace std;
 
-int main(){
+void RecieveInsideRequest(Elevator elevator,Queue queue,int requestFloorTo){
+     if(elevator.direction == "up" && requestFloorTo > elevator.current_floor)
+        elevator.ListOfStop.AscendInsert(requestFloorTo);
+     else if (elevator.direction == "down" && requestFloorTo < elevator.current_floor)
+        elevator.ListOfStop.DescendInsert(requestFloorTo);
+     else {
+         Request request = new Request("stop",requestFloorTo);
+         queue.push(requst);
+     }        
+}
 
+void RecieveOutSideRequest(Request request, Queue queue){
+    queue.push(request);
+}
+
+void pushOutSideRequestToStoppingList(Elevator elevator,Queue queue){
+     elevator.ListOfStop.pushback(queue.front());
+     queue.pop();
+     Queue TempQ = new Queue();
+     while(queue.empty()!=true){ 
+        if(queue.front().direction == "stop"){
+          if(elevator.direction == "up"&& elevator.curret_floor < queue.front().current_floor){
+             elevator.ListOfStop.AscendInsert(queue.front().current_floor;)
+          }
+          else if(elevator.direction == "down" && elevator.current_floor> queue.front().current_floor){
+             elevator.ListOfStOP.DescendInsert(queue.front().current_floor;)
+          }           
+        }
+        else if(elevator.direction == "up" && queue.front().direction == "up" && (elevator.ListOfStop.tail->data) > (queue.front().current_floor) > (elevator.current_floor) ){
+          elevator.ListOfStop.AscendInsert(queue.front().current_floor);
+          queue.front();
+        }
+        else if(elevator.direction == "down" && queue.top().direction == "down" && (elevator.ListOfStop.tail->data) < (queue.front().current_floor) < (elevator.current_floor) ){
+          elevator.ListOfStop.DescendInsert(queue.front().current_floor);
+          queue.pop();
+        }
+        else{
+          tempQ.push(queue.front());
+          queue.pop()
+        }
+     }     
+}
+
+
+
+int main(){
+   Elevator singleElevator = new Elevator();
+   Queue RequestQueue = new Queue();
+   Request request = new Request();
+ 
+   RecieveOutSideRequest(request,queue);
+   RecieveInSideRequest(singleElevator,RequestQueue,requestFloorTo);
+ 
+   if (singleElevator.ListOfStop.empty() == false){
+      if (singleElevator.current_floor==singleElevator.ListOfStop->head->data)
+          singleElevator.ListOfStop.popfront();
+      else if(singleElevator.ListOfStop.head->data > singleElevator.current_floor)
+          singleElevator.direction == "up";
+      else
+          singleElevator.direction == "down";
+   }
+ 
+   if (singleElevator.ListOfStop.empty() == true){
+      if(Request Queue.empty()!= true)
+       pushOutSideRequestToStoppingList(singleElevator,queue);
+      else
+       singleElevator.direction = "stop";
+   }
+   
+
+   
+ 
 
 
 
