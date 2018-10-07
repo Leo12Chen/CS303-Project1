8ee925875c373681c1760d34112209d7cb7eecf2
Leo12Chen <43522679+Leo12Chen@users.noreply.github.com>
2018-10-07 15:45:45 -0500

    Update Elevator.h

--- Elevator.h
+++ Elevator.h
@@ -12,8 +12,6 @@ struct Elevator {
 	string direction;                 //the direction of the Elevator is going;
 	int current_floor;                //the current floor of Elevator is at;
         List<int> ListOfStop;             //List of the floors which Elevator will stop
-	List<int> InSideRequestList;      //List of the floors which request by people in the elevator
-	Queue<int> PendingQueue;          //Queue of the floors which are pending 
 };
 
 
